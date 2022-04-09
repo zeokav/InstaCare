ed32b8be80280f16c67db94ab10ff78acedf1802
Saket Singh <sakettsingh@outlook.com>
2022-04-09 10:58:13 -0400

    Add patient consultation

--- InstaCarePlus/app/src/main/java/edu/gatech/instacareplus/ui/home/HomeFragment.kt
+++ InstaCarePlus/app/src/main/java/edu/gatech/instacareplus/ui/home/HomeFragment.kt
@@ -4,34 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.LineData
-import edu.gatech.instacareplus.MainActivity
+import edu.gatech.instacareplus.R
 import edu.gatech.instacareplus.databinding.FragmentHomeBinding
+import edu.gatech.instacareplus.ui.patient_consultation.PatientConsultationFragment
 
 class HomeFragment : Fragment() {
 
 private var _binding: FragmentHomeBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
   private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        ViewModelProvider(this).get(HomeViewModel::class.java)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-    _binding = FragmentHomeBinding.inflate(inflater, container, false)
-    val root: View = binding.root
+        return binding.root
+    }
 
-    return root
-  }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val consultationButton = view.findViewById(R.id.consultation_start_button) as Button
+        consultationButton.setOnClickListener {
+
+//            val fragment: Fragment = PatientConsultationFragment()
+//            fragment.arguments = arguments
+//            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+//            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+//            fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+//            fragmentTransaction.addToBackStack(null)
+//            fragmentTransaction.commit()
+        }
+    }
 
 override fun onDestroyView() {
         super.onDestroyView()
